c796a1ac3b1e9a368e5f02a5763b530a4c71fa4a
ShubhamFistbumpDigital <shubham.fistbump@gmail.com>
2021-07-14 16:06:48 +0530

    Grid structure for content corrected

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
   // function to slide grid letters towards with respect to the scroll position
   function SlideGridLetters() {
-    $(".grid-letters").css({ marginRight: -scrollY });
+    $(".grid-letters").css({ marginRight: -(scrollY) });
   }
 
   $(window).on("scroll", SlideGridLetters);
