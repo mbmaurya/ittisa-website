29c40ebbc0ce81470b08fe911217f19416e3352d
ShubhamIngale <32031146+ShubhamIngale@users.noreply.github.com>
2021-08-11 10:36:05 +0530

    updating code as per windows-fixing branch

--- js/main.js
+++ js/main.js
@@ -14,31 +14,34 @@ $(document).ready(function () {
     $("#toggle_menu > p").text(
       $("#toggle_menu > p").text() == "Menu" ? "Close" : "Menu"
     );
-    $("#toggle_menu").toggleClass("menu-close");
+    $("#toggle_menu").toggleClass("menu-close")
     $(".toggle").toggleClass("rotate");
     $("#navigation").toggleClass("d-block");
+
   });
 
-  // carousel
-  $(".slider").slick();
-  $(".work-slider").slick({
-    centerMode: true,
-    slidesToShow: 3,
-    responsive: [
-      {
-        breakpoint: 767,
-        settings: {
-          slidesToShow: 1,
-        },
+
+// carousel
+$(".slider").slick();
+$(".work-slider").slick({
+  centerMode: true,
+  slidesToShow: 3,
+  responsive: [
+    {
+      breakpoint: 767,
+      settings: {
+        slidesToShow: 1,
       },
-      {
-        breakpoint: 1200,
-        settings: {
-          slidesToShow: 1,
-        },
+    },
+    {
+      breakpoint: 1200,
+      settings: {
+        slidesToShow: 1,
       },
-    ],
-  });
+    },
+  ],
+});
+
 
   // accordion
   var acc = $(".accordion-btn");
@@ -57,96 +60,49 @@ $(document).ready(function () {
 
   // Initialize animation on scroll
   AOS.init();
-
-  $(".select-menu-title").click(function () {
-    $(".select-menu-options").toggleClass("open");
-    $(".section-category .dropdown-arrow").toggleClass("rotate-180");
-  });
-
-  $(".select-menu-options li").click(function () {
-    switch (this.id) {
-      case "category_1":
-        $(".category-tab-content").css("display", "none");
-        $("#category_content_1").css("display", "block");
-        break;
-
-      case "category_2":
-        $(".category-tab-content").css("display", "none");
-        $("#category_content_2").css("display", "block");
-        break;
-
-      case "category_3":
-        $(".category-tab-content").css("display", "none");
-        $("#category_content_3").css("display", "block");
-        break;
-
-      case "category_4":
-        $(".category-tab-content").css("display", "none");
-        $("#category_content_4").css("display", "block");
-        break;
-      default:
-        console.log("Unknown category");
-    }
-  });
-
-  $(".scroll-team").on("scroll", onScroll);
-
-  //smoothscroll
-  $('.tab-link a[href^="#"]').on("click", function (e) {
-    e.preventDefault();
-    $(document).off("scroll");
-
-    $("a").each(function () {
-      $(this).removeClass("active");
-    });
-    $(this).addClass("active");
-
-    var target = this.hash,
-      menu = target;
-    $target = $(target);
-    $(".scroll-team")
-      .stop()
-      .animate(
-        {
-          scrollTop: $target.offset().top,
-        },
-        0,
-        "swing",
-        function () {
-          window.location.hash = target;
-          $(document).on("scroll", onScroll);
-        }
-      );
-  });
-
-  function onScroll(event) {
-    var scrollPos = $(".team-section").scrollTop() + 400;
-    $(".tab-link a").each(function () {
-      var currLink = $(this);
-      var refElement = $(currLink.attr("href"));
-      if (
-        refElement.position().top <= scrollPos &&
-        refElement.position().top + refElement.height() > scrollPos
-      ) {
-        $(".tab-link a").removeClass("active");
-        currLink.addClass("active");
-      } else {
-        currLink.removeClass("active");
-      }
-    });
-  }
-
-  function openTab(evt, name) {
-    var i, tabContent, tabLinks;
-    tabContent = $(".tab-content");
-    for (i = 0; i < tabContent.length; i++) {
-      tabContent[i].style.display = "none";
-    }
-    tabLinks = $(".tab-link");
-    for (i = 0; i < tabLinks.length; i++) {
-      tabLinks[i].className = tabLinks[i].className.replace(" active-link", "");
-    }
-    $("#" + name).css({ display: "block" });
-    evt.currentTarget.className += " active-link";
-  }
 });
+
+
+
+// Team Tab
+
+
+$(document).ready(function () {
+    $('.scroll-team').on("scroll", onScroll);
+    
+    //smoothscroll
+    $('.tab-link a[href^="#"]').on('click', function (e) {
+        e.preventDefault();
+        $(document).off("scroll");
+        
+        $('a').each(function () {
+            $(this).removeClass('active');
+        })
+        $(this).addClass('active');
+      
+        var target = this.hash,
+            menu = target;
+        $target = $(target);
+        $('.scroll-team').stop().animate({
+            'scrollTop': $target.offset().top
+        }, 0, 'swing', function () {
+            window.location.hash = target;
+            $(document).on("scroll", onScroll);
+        });
+    });
+});
+
+function onScroll(event){
+    var scrollPos = $('.team-section').scrollTop() + 400;
+    $('.tab-link a').each(function () {
+        var currLink = $(this);
+        var refElement = $(currLink.attr("href"));
+        if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
+            $('.tab-link a').removeClass("active");
+            currLink.addClass("active");
+        }
+        else{
+            currLink.removeClass("active");
+        }
+    });
+}
